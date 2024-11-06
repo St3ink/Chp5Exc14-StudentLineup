aa6285cff6c568d146836d8a132759a67e89c617
Kason Steiner <kasonsteiner27@gmail.com>
2024-11-06 20:36:05 +0000

    Added ability to input file and organize name alphabetically. Then displays fist and last in line.

--- .ccls-cache/@home@runner@Chp5Exc14-StudentLineup/StuLineup.cpp
+++ .ccls-cache/@home@runner@Chp5Exc14-StudentLineup/StuLineup.cpp
@@ -9,35 +9,34 @@ Write a program that prompts the user to enter the number of students in the cla
 Once all the names have been read, it reports which student would be at the front of the line and
 which one would be at the end of the line. You may assume that no two students have the same name.
 Input Validation: Do not accept a number less than 1 or greater than 25 for the number of students.
+Modify program to function the same but, for it allow names to be read from a file and display first and last in line.
 */  
 #include <iostream>
 #include <string>
 #include <vector> 
+#include <fstream>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-  int students;
-      cout << "How many students are in your class? ";
-      cin >> students;
+  string studentName;
+  ifstream inFile;
 
-  while (students < 1 || students > 25)
-  {
-    cout << "Invalid input. Please enter a number btetween 1 and 25: ";
-    cin >> students;
-  }
 
-  vector<string> name(students);
+  inFile.open("names.txt");
 
-  cout << "Enter the name of student: \n";
+ vector<string> name(65); //65 students in class to get names of
 
-  for (int count = 0; count < students; ++count)
+  for (int count = 0; count < 65; ++count)
     {
-      cin >> name[count];
+      inFile >> name[count];
     }
 
-  cout << "\nThe first student in line is " << name[0] << endl;
-  cout << "The student at the end of the line is " << name[students - 1] << endl;
+    sort(name.begin(), name.end()); //Sort vector alphabetically
+
+  cout << "\nThe first student in line is " << name.front() << endl; //displays first student in line
+  cout << "The student at the end of the line is " << name.back() << endl; //displays last student in line
 
 return 0;
 }
